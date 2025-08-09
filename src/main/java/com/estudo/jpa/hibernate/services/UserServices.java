d70df9ffe3f5d7fbe8e0dcf7835f755c7c7b68c5
Pedro Bastos <henriquepedro.2001@gmail.com>
2025-08-09 17:07:18 -0300

    feat: implement user service layer and complete endpoints

---
+++ src/main/java/com/estudo/jpa/hibernate/services/UserServices.java
@@ -0,0 +1,24 @@
+package com.estudo.jpa.hibernate.services;
+
+import com.estudo.jpa.hibernate.entities.User;
+import com.estudo.jpa.hibernate.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServices {
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
