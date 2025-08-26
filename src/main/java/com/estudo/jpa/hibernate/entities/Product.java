04ead9bdd06917c62cbd6a4319b9236da4ddd574
Pedro Bastos <henriquepedro.2001@gmail.com>
2025-08-26 10:09:27 -0300

    feat: implement Payment with one-to-one association

--- src/main/java/com/estudo/jpa/hibernate/entities/Product.java
+++ src/main/java/com/estudo/jpa/hibernate/entities/Product.java
@@ -25,7 +25,7 @@ public class Product implements Serializable {
             joinColumns = @JoinColumn(name = "product_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
-    
+
     @OneToMany(mappedBy = "id.product")
     private Set<OrderItem> items = new HashSet<>();
 
