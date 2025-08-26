2eab175a69041927fe5016c256877dd46234f549
Pedro Bastos <henriquepedro.2001@gmail.com>
2025-08-26 20:23:00 -0300

    feat: implement Exception handling - findById

--- src/main/java/com/estudo/jpa/hibernate/services/UserServices.java
+++ src/main/java/com/estudo/jpa/hibernate/services/UserServices.java
@@ -2,6 +2,7 @@ package com.estudo.jpa.hibernate.services;
 
 import com.estudo.jpa.hibernate.entities.User;
 import com.estudo.jpa.hibernate.repositories.UserRepository;
+import com.estudo.jpa.hibernate.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +20,7 @@ public class UserServices {
 
     public User findById(Long id){
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(()-> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
