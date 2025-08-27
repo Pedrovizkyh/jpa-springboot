4c40d4a0386dde08676af108052cb277fb3de4b6
Pedro Bastos <henriquepedro.2001@gmail.com>
2025-08-26 21:12:45 -0300

    feat: implement Exception handling - delete

--- src/main/java/com/estudo/jpa/hibernate/services/UserServices.java
+++ src/main/java/com/estudo/jpa/hibernate/services/UserServices.java
@@ -2,8 +2,11 @@ package com.estudo.jpa.hibernate.services;
 
 import com.estudo.jpa.hibernate.entities.User;
 import com.estudo.jpa.hibernate.repositories.UserRepository;
+import com.estudo.jpa.hibernate.services.exceptions.DatabaseException;
 import com.estudo.jpa.hibernate.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -28,7 +31,14 @@ public class UserServices {
     }
 
     public void delete(Long id){
-        repository.deleteById(id);
+        repository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id));
+        try {
+            repository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+
+
     }
 
     public User update(Long id, User obj){
